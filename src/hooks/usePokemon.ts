7b35a72fcde7c1625023185f8ed2f51d21cf0efe
Laureano Serrano <serranolaureano.220@gmail.com>
2023-06-21 03:05:09 -0300

    remove set type undefined

--- src/hooks/usePokemon.ts
+++ src/hooks/usePokemon.ts
@@ -1,23 +1,25 @@
-import { useEffect, useState } from "react"
-import { IPokemon } from "../interfaces/interfaces"
-import axios from "axios"
+import { useEffect, useState } from "react";
+import { IPokemon } from "../interfaces/interfaces";
+import axios from "axios";
 
 export const usePokemon = (url?: string, id?: string) => {
-    const [pokemon, setPokemon] = useState<null | undefined | IPokemon>()
+  const [pokemon, setPokemon] = useState<null | IPokemon>();
 
-    const fetchPokemon = async () => {
-        if(url){
-            const {data} = await axios.get(url)
-            setPokemon(data)
-        }else if(id){
-            const {data} = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
-            setPokemon(data)
-        }
+  const fetchPokemon = async () => {
+    if (url) {
+      const { data } = await axios.get(url);
+      setPokemon(data);
+    } else if (id) {
+      const { data } = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${id}`
+      );
+      setPokemon(data);
     }
+  };
 
-    useEffect(() =>{
-        fetchPokemon();
-    }, [])
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
 
-    return { pokemon };
-}
+  return { pokemon };
+};
